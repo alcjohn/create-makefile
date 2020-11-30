e7369b93cb68c2add3afe5545f90de725006cf68
John Bioux <john.bioux@gmail.com>
2020-11-30 11:28:41 +0100

    fix path template

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@ import fs from 'fs-extra';
 import path from 'path';
 
 export async function cli(args) {
-	await fs.copy(
-		path.join(__dirname, 'makefile'),
-		path.join(process.env.PWD, 'makefile')
-	);
+  await fs.copy(
+    path.join(__dirname, '..', 'templates', 'makefile'),
+    path.join(process.env.PWD, 'makefile')
+  );
 }
